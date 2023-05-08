1247112c263b07360f449217db36e87aeb9677c9
shusaku009 <shusaku009@gmail.com>
2023-05-08 20:05:52 +0900

    fix ciでrspecがエラーになる問題を修正 #1

--- spec/requests/samples_spec.rb
+++ spec/requests/samples_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe("Samples", type: :system) do
-  describe "Capybaraの疎通確認用" do
+RSpec.describe('Samples', type: :system) do
+  describe 'Capybaraの疎通確認用' do
     it 'ページが開ける' do
       visit '/'
       expect(page).to have_content 'テストです'
