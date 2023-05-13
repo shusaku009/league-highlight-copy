4dae1a248849d4db3d1727af145d21ced1feb0a1
shusaku009 <shusaku009@gmail.com>
2023-05-13 16:35:21 +0900

    add タイムゾーンを設定 #5

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module LeagueHighlight
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
